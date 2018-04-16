60c4bf41e715105dcca0f3361c7ed72bd3c3cf99
CNHume <cnhume-software@yahoo.com>
2018-04-16 00:29:59 -0800

    Updated QuickSortPostedTripartite.cs

--- Sort/QuickSort/QuickSortPostedTripartite.cs
+++ Sort/QuickSort/QuickSortPostedTripartite.cs
@@ -6,15 +6,22 @@
 
 namespace Sort {
   using System;
+  using System.Diagnostics;
 
   class QuickSort<T> where T : IComparable {
     #region Constants
-    private const Int32 insertionLimitDefault = 12;
+    private const Int32 INSERTION_LIMIT_DEFAULT = 12;
     #endregion
 
     #region Properties
     public Int32 InsertionLimit { get; set; }
     protected Random Random { get; set; }
+
+    private T Median { get; set; }
+    private Int32 Left { get; set; }
+    private Int32 Right { get; set; }
+    private Int32 LeftMedian { get; set; }
+    private Int32 RightMedian { get; set; }
     #endregion
 
     #region Constructors
@@ -24,7 +31,7 @@ namespace Sort {
     }
 
     public QuickSort()
-      : this(insertionLimitDefault, new Random()) {
+      : this(INSERTION_LIMIT_DEFAULT, new Random()) {
     }
     #endregion
 
@@ -41,97 +48,102 @@ namespace Sort {
           return;
         }
 
-        var median = pivot(entries, first, last);
+        Left = first;
+        Right = last;
+        pivot(entries);
+        partition(entries);
+        //[Note]Right < Left
 
-        var left = first;
-        var right = last;
-        partition(entries, median, ref left, ref right);
-
-        var leftLength = right + 1 - first;
-        var rightLength = last + 1 - left;
+        var leftLength = Right + 1 - first;
+        var rightLength = last + 1 - Left;
 
         //
         // First recurse over shorter partition, then loop
         // on the longer partition to elide tail recursion.
         //
         if (leftLength < rightLength) {
-          Sort(entries, first, right);
-          first = left;
+          Sort(entries, first, Right);
+          first = Left;
           length = rightLength;
         }
         else {
-          Sort(entries, left, last);
-          last = right;
+          Sort(entries, Left, last);
+          last = Right;
           length = leftLength;
         }
       }
     }
 
-    private static void partition(T[] entries, T median, ref Int32 left, ref Int32 right) {
-      var first = left;
-      var last = right;
-#if Tripartite
-      var leftMedian = first;
-      var rightMedian = last;
-#endif
-      while (true) {
-        //[Assert]There exists some index >= left where entries[index] >= median
-        //[Assert]There exists some index <= right where entries[index] <= median
-        // So, there is no need for left or right bound checks
-        while (median.CompareTo(entries[left]) > 0) left++;
-        while (median.CompareTo(entries[right]) < 0) right--;
-
-        //[Assert]entries[right] <= median <= entries[left]
-        if (right <= left) break;
-
-        Swap(entries, left, right);
-#if Tripartite
-        // Bentley-McIlroy 3-way Partitioning: Put median entries aside
-        if (median.CompareTo(entries[left]) == 0) Swap(entries, leftMedian++, left);
-        if (median.CompareTo(entries[right]) == 0) Swap(entries, right, rightMedian--);
-#endif
-        left++;
-        right--;
-        //[Assert]entries[first:left - 1] <= median <= entries[right + 1:last]
-      }
-
-      if (left == right) {
-        left++;
-        right--;
-      }
-      //[Assert]right < left
-#if Tripartite
-      // Restore median entries
-      for (var prefix = first; prefix < leftMedian;)
-        Swap(entries, prefix++, right--);
-      for (var suffix = last; rightMedian < suffix;)
-        Swap(entries, left++, suffix--);
-#endif
-      //[Assert]entries[first:right] <= median <= entries[left:last]
-      //[Assert]entries[right + 1:left - 1] == median when non-empty
-    }
-
-    private T pivot(T[] entries, Int32 first, Int32 last) {
+    private void pivot(T[] entries) {
       //
       // An odd sample size is chosen based on the log of the interval size.
       // The median of a randomly chosen set of samples is then returned as
       // an estimate of the true median.
       //
-      var length = last + 1 - first;
+      var length = Right + 1 - Left;
       var logLen = (Int32)Math.Log10(length);
       var pivotSamples = 2 * logLen + 1;
       var sampleSize = Math.Min(pivotSamples, length);
-      var right = first + sampleSize - 1;
+      var last = Left + sampleSize - 1;
       // Sample without replacement
-      for (var left = first; left <= right; left++) {
+      for (var first = Left; first <= last; first++) {
         // Random sampling avoids pathological cases
-        var random = Random.Next(left, last + 1);
-        Swap(entries, left, random);
+        var random = Random.Next(first, Right + 1);
+        Swap(entries, first, random);
       }
 
-      InsertionSort<T>.Sort(entries, first, right);
-      var median = entries[first + sampleSize / 2];
-      return median;
+      InsertionSort<T>.Sort(entries, Left, last);
+      Median = entries[Left + sampleSize / 2];
+    }
+
+    private void partition(T[] entries) {
+      var first = Left;
+      var last = Right;
+#if Tripartite
+      LeftMedian = first;
+      RightMedian = last;
+#endif
+      while (true) {
+        //[Assert]There exists some index >= Left where entries[index] >= Median
+        //[Assert]There exists some index <= Right where entries[index] <= Median
+        // So, there is no need for Left or Right bound checks
+        while (Median.CompareTo(entries[Left]) > 0) Left++;
+        while (Median.CompareTo(entries[Right]) < 0) Right--;
+
+        //[Assert]entries[Right] <= Median <= entries[Left]
+        if (Right <= Left) break;
+
+        Swap(entries, Left, Right);
+        swapOut(entries);
+        Left++;
+        Right--;
+        //[Assert]entries[first:Left - 1] <= Median <= entries[Right + 1:last]
+      }
+
+      if (Left == Right) {
+        Left++;
+        Right--;
+      }
+      //[Assert]Right < Left
+      swapIn(entries, first, last);
+
+      //[Assert]entries[first:Right] <= Median <= entries[Left:last]
+      //[Assert]entries[Right + 1:Left - 1] == Median when non-empty
+    }
+    #endregion
+
+    #region Swap Methods
+    [Conditional("Tripartite")]
+    private void swapOut(T[] entries) {
+      if (Median.CompareTo(entries[Left]) == 0) Swap(entries, LeftMedian++, Left);
+      if (Median.CompareTo(entries[Right]) == 0) Swap(entries, Right, RightMedian--);
+    }
+
+    [Conditional("Tripartite")]
+    private void swapIn(T[] entries, Int32 first, Int32 last) {
+      // Restore Median entries
+      while (first < LeftMedian) Swap(entries, first++, Right--);
+      while (RightMedian < last) Swap(entries, Left++, last--);
     }
 
     public static void Swap(T[] entries, Int32 index1, Int32 index2) {
