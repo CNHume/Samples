c00cc13b89d162d9ef4401dd084a376e64023d8b
CNHume <cnhume-software@yahoo.com>
2018-12-23 09:33:22 -0800

    Added Command class.

---
+++ CSharp/Search/Anagram/Command.cs
@@ -0,0 +1,62 @@
+﻿//
+// Copyright (C) 2018, Christopher N. Hume.  All rights reserved.
+//
+// You should have received a copy of the MIT License along with this program.
+// If not, see https://opensource.org/licenses/MIT.
+//
+// 2018-12-23 CNHume  Added Command class
+//
+namespace Anagram {
+  using System;
+
+  public class Command {
+    #region Virtual Fields
+    public string Letters;
+    public bool Subset;
+    #endregion
+
+    #region Methods
+    public void Parse(string[] args) {
+      Letters = null;
+      Subset = false;
+
+      var usage = false;
+      var count = args.Length;
+      var n = 0;
+      for (; n < count && !usage; n++) {
+        var token = args[n];            // parse any switches
+        var len = token.Length;
+        if (len < 1 || token[0] != '-')
+          break;                        // end of switches
+        else if (len < 2)
+          usage = true;
+        else {                          // parse single character switch
+          switch (token[1]) {
+          case 's':                     // the subset switch
+            if (len > 2)                // superfluous value specified
+              usage = true;
+            else
+              Subset = true;
+            break;
+
+          default:                      // switch unknown
+            usage = true;
+            break;
+          }
+        }
+      }
+
+      // Require Letters
+      if (count <= n)
+        usage = true;
+      else
+        Letters = args[n++];
+
+      usage |= n < count;               // superfluous argument specified
+
+      if (usage)                        // throw usage line if parse failed
+        throw new ApplicationException("Usage: Anagram [-s] letters");
+    }
+    #endregion
+  }
+}
--- CSharp/Search/Anagram/Program.cs
+++ CSharp/Search/Anagram/Program.cs
@@ -1,13 +1,18 @@
 ﻿//
 // Copyright (C) 2018, Christopher N. Hume.  All rights reserved.
 //
+// You should have received a copy of the MIT License along with this program.
+// If not, see https://opensource.org/licenses/MIT.
+//
 //[2018-12-22 CNHume]Created Anagram (.Net Core) Console Application
 //
 // To publish:
 // dotnet publish Anagram -c Release -r win10-x64
 //
 // To run:
-// Anagram\bin\Release\netcoreapp2.1\win10-x64\publish>Anagram.exe abc...
+// Anagram\bin\Release\netcoreapp2.1\win10-x64\publish\Anagram abc...
+//
+// Usage: Anagram [-s] letters
 //
 namespace Anagram {
   using System;
@@ -17,15 +22,19 @@ namespace Anagram {
     #region Methods
     private static void Main(string[] args) {
       try {
-        if (args.Length > 0) {
-          var letters = args[0];
-          Console.WriteLine($"Leters: {letters}");
-          var words = Anagram(letters, true);
-          Console.WriteLine($"{words.Count} anagrams found:");
-          foreach (var word in words) {
-            Console.WriteLine(word);
-          }
+        var cmd = new Command();
+        cmd.Parse(args);
+
+        Console.WriteLine($"Leters: {cmd.Letters}");
+        var words = Anagram(cmd.Letters, cmd.Subset);
+
+        Console.WriteLine($"{words.Count} anagrams found:");
+        foreach (var word in words) {
+          Console.WriteLine(word);
         }
+      }
+      catch (ApplicationException ex) {
+        Console.WriteLine(ex.Message);
       }
       catch (Exception ex) {
         Console.WriteLine(ex.ToString());
