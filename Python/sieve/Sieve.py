966ae8bb5365e7cbcb7a2b1511f36803f1311ab3
CNHume <cnhume-software@yahoo.com>
2019-08-05 04:01:12 -0800

    Completed Incremental Generation

--- Python/sieve/Sieve.py
+++ Python/sieve/Sieve.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019, Christopher Hume.  All rights reserved.
-# 2015-05-04  CNHume  Created File
+# 2019-08-04  CNHume  Completed Incremental Generation
+# 2015-05-04  CNHume  Created Prime Number Generator
 class Sieve:
   '''Sieve of Eratosthenes'''
   def __init__(self):
-    '''Obtain indexes for the odd composites such that n = 2 * index + 1 < limit'''
-    self.limit = 1
+    '''Incremental Prime Number Generator'''
+    # Odd Composite Indexes such that n = 2 * index + 1 < limit
+    self.sieveIndexes = set()           
+
+    self.limitPrimes = []
+    self.sievePrimes = []
+    self.primeLimit = 1
+    self.sieveLimit = 1
+
     self.oddIndex = 0
     self.odd = 1
     self.delta = 0
     self.squareIndex = 0
     self.square = 1
-
-    self.sieve = set()
-    self.siftPrimes = []
-    self.listPrimes = []
   
   def sift(self):
     '''Sift Composites less than limit'''
@@ -29,16 +33,16 @@ class Sieve:
     self.square += self.delta + self.delta
 
     # Test whether odd is Prime
-    if self.oddIndex not in self.sieve:
+    if self.oddIndex not in self.sieveIndexes:
       p = 2 * self.oddIndex + 1
-      print('Appending {}'.format(p))
-      self.siftPrimes.append(p)
+      #[Test]print('Appending {}'.format(p))
+      self.sievePrimes.append(p)
       # Sift odd multiples of odd
-      for oddIndex2 in range(self.squareIndex, self.limit // 2, self.odd):
-        self.sieve.add(oddIndex2)
+      for oddIndex2 in range(self.squareIndex, self.sieveLimit // 2, self.odd):
+        self.sieveIndexes.add(oddIndex2)
 
   def rangeStart(self, p):
-    limit = self.limit
+    limit = self.sieveLimit
     pp = p * p
     if pp < limit:
       p2 = p + p
@@ -50,29 +54,30 @@ class Sieve:
     return p, start
 
   def raiseLimit(self, limit):
-    pairs = map(self.rangeStart, self.siftPrimes)
+    pairs = map(self.rangeStart, self.sievePrimes)
     for p, start in pairs:
       for oddIndex2 in range(start // 2, limit // 2, p):
-        self.sieve.add(oddIndex2)
-    self.limit = limit
+        self.sieveIndexes.add(oddIndex2)
+    self.sieveLimit = limit
 
-  def genPrimes(self, limit):
+  def genPrimes(self, start, limit):
     '''Generate Primes less than limit'''
-    if self.limit < limit:
-      self.listPrimes = []
+    if self.sieveLimit < limit:
       self.raiseLimit(limit)
 
-    while self.square < self.limit:
+    while self.square < self.sieveLimit:
       self.sift()
 
-    for oddIndex in range(self.limit // 2):
-      if oddIndex not in self.sieve:
+    for oddIndex in range(start // 2, self.sieveLimit // 2):
+      if oddIndex not in self.sieveIndexes:
         # Repurpose the index corresponding to 1 to represent 2 instead,
         # replacing the multiplicative identity with the sole even Prime
         yield 2 * oddIndex + 1 if oddIndex > 0 else 2
 
   def primes(self, limit):
     '''Return Primes less than limit'''
-    if self.limit < limit:
-      self.listPrimes = list(self.genPrimes(limit))
-    return self.listPrimes
+    if self.primeLimit < limit:
+      for p in self.genPrimes(self.primeLimit, limit):
+        self.limitPrimes.append(p)
+      self.primeLimit = limit
+    return self.limitPrimes
