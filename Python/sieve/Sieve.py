27c631aa9c9c6403178b9ca91bdc8bab72baab0b
CNHume <cnhume-software@yahoo.com>
2019-09-03 22:12:38 -0800

    Renamed methods.  Advance over successive squares within expand().  Made lastSquare a local.

--- Python/sieve/Sieve.py
+++ Python/sieve/Sieve.py
@@ -21,48 +21,48 @@ class Sieve:
     self.oddSifted = 1
     self.delta = 0
     self.square = 1
-    self.lastSquare = 1
     
-  def nextSquare(self, limit):
-    '''Advance to next Square, extending sievePrimes'''
-    # Test whether odd is Prime
-    if self.odd > 1 and self.oddIndex not in self.sieveIndexes:
+  def expand(self, limit):
+    '''Advance over successive squares, expanding sievePrimes'''
+    while self.square < limit:
+      # Test whether odd is Prime
+      if self.odd > 1 and self.oddIndex not in self.sieveIndexes:
+        if self.debug:
+          print('sievePrimes.append({})'.format(self.odd))
+        self.sievePrimes.append(self.odd)
+        for index in range(self.square // 2, limit // 2, self.odd):
+          self.sieveIndexes.add(index)
+
+      self.oddIndex += 1
+      self.odd += 2
+      self.delta += 4
+
+      # Note: square receives the value of odd squares: 1, 9, 25, 49, 81...
+      # The difference between the nth odd square and its successor is 8*n
+      # because odd n increase by 2 and (n + 2)**2 - n**2 = 4*n + 4
+      self.square += self.delta + self.delta
       if self.debug:
-        print('sievePrimes.append({})'.format(self.odd))
-      self.sievePrimes.append(self.odd)
-      for index in range(self.square // 2, limit // 2, self.odd):
-        self.sieveIndexes.add(index)
-
-    self.oddIndex += 1
-    self.odd += 2
-    self.delta += 4
-
-    # Note: square receives the value of odd squares: 1, 9, 25, 49, 81...
-    # The difference between the nth odd square and its successor is 8*n
-    # because odd n increase by 2 and (n + 2)**2 - n**2 = 4*n + 4
-    self.square += self.delta + self.delta
-    if self.debug:
-      print('square = {}'.format(self.square))
+        print('square = {}'.format(self.square))
   
   def nextMuliple(self, p):
-    '''Return next odd multiple of p greater than or equal to lastSquare'''
+    '''Return next odd multiple of p greater than or equal to square'''
     # Skip even multiples of p
     m = p + p
     # The next multiple is congruent delta mod m
-    delta = p * p - self.lastSquare
-    next = self.lastSquare + delta % m
+    delta = p * p - self.square
+    next = self.square + delta % m
     return p, next
 
-  def sift(self, limit):
-    '''Sift out odd composites | lastSquare < n = 2 * index + 1 < limit'''
+  def extend(self, limit):
+    '''Sift out odd composites | square < n = 2 * index + 1 < limit'''
     pairs = map(self.nextMuliple, self.sievePrimes)
     for p, next in pairs:
       for index in range(next // 2, limit // 2, p):
         self.sieveIndexes.add(index)
 
-  def sifted(self):
+  def sifted(self, lastLimit, limit):
     '''Next sifted Prime'''
-    for oddIndex in range(self.lastSquare // 2, self.square // 2):
+    for oddIndex in range(lastLimit // 2, limit // 2):
       self.oddSifted = 2 * oddIndex + 1
       if oddIndex not in self.sieveIndexes:
         # Re-purpose the index corresponding to 1 to represent 2 instead,
@@ -70,21 +70,19 @@ class Sieve:
         p = self.oddSifted if oddIndex > 0 else 2
         yield p
 
-  def setSquare(self, limit):
+  def sift(self, limit):
     nextRoot = Sieve.isqrt(limit - 1) + 1
     nextOdd = nextRoot | 1
     nextSquare = nextOdd * nextOdd
-    self.sift(nextSquare)
-    while self.square < nextSquare:
-      self.nextSquare(nextSquare)
+    self.extend(nextSquare)
+    self.expand(nextSquare)
 
   def primes(self, limit):
     '''Return Primes less than limit'''
-    self.setSquare(limit)
-    for p in self.sifted():
+    lastSquare = self.square
+    self.sift(limit)
+    for p in self.sifted(lastSquare, self.square):
       self.squarePrimes.append(p)
-    # lastSquare is used by nextMuliple()
-    self.lastSquare = self.square
     result = [p for p in self.squarePrimes if p < limit] if limit < self.square else self.squarePrimes
     return result
 
