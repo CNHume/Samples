fc76f2a3edb8519ae00e8c49a644f96c647ed682
CNHume <cnhume-software@yahoo.com>
2019-09-06 05:17:36 -0800

    Completed nextPrime() generator.  Unrolled sift().  Renamed leastUpperSquare() and refactored nextSquare() out from expand().

--- Python/sieve/Sieve.py
+++ Python/sieve/Sieve.py
@@ -13,33 +13,23 @@ class Sieve:
     # sievePrimes are used by extend() to find nextMuliple()
     self.sievePrimes = []
     self.squarePrimes = []
-
+    self.count = 0
     self.oddIndex = 0
     self.odd = 1
     self.delta = 0
     self.square = 1
-    
-  def expand(self, limit):
-    '''Expand sieve while square < limit'''
-    while self.square < limit:
-      # Test whether odd is Prime
-      if self.odd > 1 and self.oddIndex not in self.sieveIndexes:
-        if self.debug:
-          print('sievePrimes.append({})'.format(self.odd))
-        self.sievePrimes.append(self.odd)
-        for index in range(self.square // 2, limit // 2, self.odd):
-          self.sieveIndexes.add(index)
 
-      self.oddIndex += 1
-      self.odd += 2
-      self.delta += 4
-
-      # Note: square receives the value of odd squares: 1, 9, 25, 49, 81...
-      # The difference between the nth odd square and its successor is 8*n
-      # because odd n increase by 2 and (n + 2)**2 - n**2 = 4*n + 4
-      self.square += self.delta + self.delta
-      if self.debug:
-        print('square = {}'.format(self.square))
+  def nextSquare(self):
+    '''Advance to next square'''
+    self.oddIndex += 1
+    self.odd += 2
+    self.delta += 4
+    # Note: square receives the value of odd squares: 1, 9, 25, 49, 81...
+    # The difference between the nth odd square and its successor is 8*n
+    # because odd n increase by 2 and (n + 2)**2 - n**2 = 4*n + 4
+    self.square += self.delta + self.delta
+    if self.debug:
+      print('square = {}'.format(self.square))
   
   def leastMuliple(self, limit, p):
     '''Least odd multiple of p greater than or equal to limit'''
@@ -58,33 +48,53 @@ class Sieve:
       for p, next in pairs:
         for index in range(next // 2, nextLimit // 2, p):
           self.sieveIndexes.add(index)
+    
+  def expand(self, limit):
+    '''Expand sieve to next limit'''
+    # Test whether odd is Prime
+    if self.odd > 1 and self.oddIndex not in self.sieveIndexes:
+      if self.debug:
+        print('sievePrimes.append({})'.format(self.odd))
+      self.sievePrimes.append(self.odd)
+      for index in range(self.square // 2, limit // 2, self.odd):
+        self.sieveIndexes.add(index)
 
   def sifted(self, lastLimit, nextLimit):
     '''Generate sifted Primes'''
     for oddIndex in range(lastLimit // 2, nextLimit // 2):
       if oddIndex not in self.sieveIndexes:
+        self.count += 1
         # Re-purpose the index corresponding to 1 to represent 2 instead,
         # replacing the multiplicative identity with the sole even Prime
         p = 2 * oddIndex + 1 if oddIndex > 0 else 2
         yield p
 
-  def sift(self, limit):
-    '''Extend and expand sieve to new limit'''
-    self.extend(self.square, limit)
-    self.expand(limit)
-
   def primes(self, limit):
     '''Return Primes less than limit'''
     lastSquare = self.square
-    nextSquare = Sieve.nextSquare(limit)
-    self.sift(nextSquare)
+    nextSquare = Sieve.leastUpperSquare(limit)
+    self.extend(lastSquare, nextSquare)
+    while self.square < nextSquare:
+      self.expand(nextSquare)
+      self.nextSquare()
     for p in self.sifted(lastSquare, nextSquare):
       self.squarePrimes.append(p)
     result = [p for p in self.squarePrimes if p < limit] if limit < nextSquare else self.squarePrimes
     return result
 
+  def nextPrime(self):
+    '''Generate next Prime'''
+    while True:
+      lastSquare = self.square
+      self.nextSquare()
+      nextSquare = self.square
+      self.extend(lastSquare, nextSquare)
+      self.expand(nextSquare)
+      for p in self.sifted(lastSquare, nextSquare):
+        yield p
+
   @staticmethod
-  def nextSquare(n):
+  def leastUpperSquare(n):
     nextRoot = Sieve.isqrt(n - 1) + 1
     nextOdd = nextRoot | 1
     return nextOdd * nextOdd
