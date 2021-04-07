6afe69751f050c06523bbe7edb765252d177fd4f
Christopher Hume <cnhume-software@yahoo.com>
2021-04-07 16:20:41 -0700

    Added a Column parameter to the Rewind(Row, Column) method and added a position overload and property consisting of (Row, Column) pairs.  Simplified EndOfLine logic.  Made NextRow a private property.

--- CSharp/Square/Square/Command/Scanner/Scanner.cs
+++ CSharp/Square/Square/Command/Scanner/Scanner.cs
@@ -31,27 +31,24 @@ namespace Command {
     private Boolean disposed = false;
     private TextReader reader;
     private String sText;
-    private Int32 nNextRow;
     private Boolean bEndOfLine;
     #endregion
 
     #region Properties
+    private Int32 NextRow { get; set; }
     public Int32 Row { get; private set; }
-    public Int32 Column { get; set; }
+    public Int32 Column { get; private set; }
+    public (Int32, Int32) Position { get => (Row, Column); }
 
     // The following works for TextReader as well as StreamReader:
     public Boolean EndOfStream { get => Text is null; }
-    public Boolean EndOfLine {
-      get => bEndOfLine || EndOfStream;
-      private set => bEndOfLine = value;
-    }
+    public Boolean EndOfLine { get => IsNullOrEmpty(Text); }
 
     public TextReader Reader {
       get => reader;
       set {
         Rows.Clear();
-        EndOfLine = false;
-        nNextRow = 0;
+        NextRow = 0;
         Column = 0;
         reader?.Dispose();
         reader = value;
@@ -79,8 +76,7 @@ namespace Command {
 
     #region Constructors
     public Scanner() {
-      EndOfLine = false;
-      nNextRow = 0;
+      NextRow = 0;
       Rows = new List<String>(nRowsCapacity);
     }
 
@@ -112,7 +108,7 @@ namespace Command {
     }
 
     public void ReadLine() {
-      Row = nNextRow++;
+      Row = NextRow++;
       Column = 0;
 
       if (Row < Rows.Count)
@@ -123,18 +119,28 @@ namespace Command {
       }
     }
 
-    public virtual void Rewind(Int32 nRow = 0) {
+    public virtual void Rewind(Int32 nRow = 0, Int32 nColumn = 0) {
       if (Rows.Count <= nRow)
-        throw new ParseException($"Row = {nRow} must be < Rows.Count = {Rows.Count}");
-      EndOfLine = false;
-      nNextRow = nRow;
+        throw new ArgumentException($"Row = {nRow} must be < Rows.Count = {Rows.Count}", nameof(nRow));
+
+      var nLength = Rows[nRow].Length;
+      if (nLength <= nColumn)
+        throw new ArgumentException($"Column = {nColumn} must be < Rows[{nRow}].Length = {nLength}", nameof(nColumn));
+
+      NextRow = nRow;
       ReadLine();
+
+      if (nColumn > 0)
+        Skip(nColumn);
+    }
+
+    public void Rewind((Int32 nRow, Int32 nColumn) position) {
+      Rewind(position.nRow, position.nColumn);
     }
 
     public void Skip(Int32 nLength) {
       Column += nLength;
       Text = Text.Substring(nLength);
-      EndOfLine = Text.Length == 0;
       if (EndOfLine) ReadLine();
     }
 
@@ -213,23 +219,22 @@ namespace Command {
 
     #region Methods
     //
-    //[Note]Reader.BaseStream.Position property cannot be used to implement random access Seek() for StreamScanner.
-    // The BaseStream manages an internal buffer which supports alternative Character Encodings.
+    //[Note]The Reader.BaseStream.Position property cannot be used to implement random access Seek()
+    // for StreamScanner, as follows:
     //
-    // It will be much more practical to base random access on the Scanner base class Row and Column properties.
+    // Discard unread characters whenever BaseStream.Position is modified.
+    // Reader.DiscardBufferedData();
+    // Reader.BaseStream.Position = 0;
+    //
+    // This is because BaseStream manages an internal buffer in order to support Character Encodings.
+    // Thus, one must implement Rewind() via Row and Column properties.
     //
     // See related discussion on Stackoverflow of "Tracking the position of the line of a streamreader"
     // https://stackoverflow.com/questions/10189270/tracking-the-position-of-the-line-of-a-streamreader
     //
-    public override void Rewind(Int32 nRow = 0) {
-      //
-      // Discard unread characters whenever BaseStream.Position is modified.
-      //
-      //Reader.DiscardBufferedData();
-      //Reader.BaseStream.Position = 0;
-
-      base.Rewind(nRow);
-    }
+    //public override void Rewind(Int32 nRow = 0, Int32 nColumn = 0) {
+    //  base.Rewind(nRow, nColumn);
+    //}
     #endregion
   }
 }
