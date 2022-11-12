fd348935e4a12339a6ea4ecc71bf08b7ead5f765
Christopher Hume <cnhume-software@yahoo.com>
2022-11-12 11:35:43 -0800

    Updated timing results to reflect .Net 7.0 performance improvements.

--- C#/Square/Square/Command/UCI/UCIProperty.cs
+++ C#/Square/Square/Command/UCI/UCIProperty.cs
@@ -18,11 +18,11 @@ namespace Command {
     //
     // Perft FEN
     // ---------
-    //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" // [2022-11-03 Perft1 (startpos) in 14.329 sec @9.098 MHz over 130.36 Mnode]
-    //"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"; // [2022-11-03 Perft2 in 22.35 sec @9.2 MHz over 205.63 Mnode]
-    //"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"; // [2022-11-03 Perft3 in 29.18 sec @7.5 MHz over 218.84 Mnode]
-    //"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"; // [2022-11-03 Perft4 in 1:32.84 @8.264 MHz over 767.28 Mnode]
-    //"n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1"; // [2022-11-03 Perft5 in 13.363 sec @6.556 MHz over 87.6 Mnode]
+    //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" // [2022-11-12 Perft1 (startpos) in 13.868 sec, @9.4 MHz over 130.36 Mnode]
+    //"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"; // [2022-11-12 Perft2 in 21.744 sec, @9.457 MHz over 205.63 Mnode]
+    //"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"; // [2022-11-12 Perft3 in 28.379 sec, @7.71 MHz over 218.84 Mnode]
+    //"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"; // [2022-11-12 Perft4 in 1:30.38 sec, @8.49 MHz over 767.28 Mnode]
+    //"n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1"; // [2022-11-12 Perft5 in 12.662 sec, @6.919 MHz over 87.6 Mnode]
     //
     // Abbreviation Tests
     // ------------------
