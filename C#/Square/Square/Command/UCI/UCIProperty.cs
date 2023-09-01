07cabbebd4cceb3604e60f3befb460a3e3656c35
Christopher Hume <cnhume-software@yahoo.com>
2023-08-31 21:27:13 -0700

    Added Kasparov v Ribli 1989 Ending.

--- C#/Square/Square/Command/UCI/UCIProperty.cs
+++ C#/Square/Square/Command/UCI/UCIProperty.cs
@@ -323,10 +323,15 @@ partial class UCI {
   // [13-ply in 3:52.4 @1.323 MHz] eval 0.87
   //"r1qr2k1/1p3pp1/2pbbn2/p3N3/3P3Q/P5PB/1B3P2/R3R1K1 w - - 0 1"; // Taimanov v Kuzminykh 1950 [13-ply in 1:40 @1.472 over 161.9 Mnode] eval 2.35,
   // [14-ply in 5:59.1 @1.3865 MHz over 497.9 Mnode] eval 2.35, [15-ply in 15:59 @1.387 MHz over 1.33 Gnode] eval 2.55
-  //"5rk1/5ppp/p1Q1p3/1r6/q2b4/4B1P1/P2RPP1P/1R4K1 w - - 0 26"; // Kasparov v Ribli 1989 [2023-08-31 20-ply in 1:54:41 @1.366 MHz over 9.401 Gnode] eval 0.0
+  //"5rk1/5ppp/p1Q1p3/1r6/q2b4/4B1P1/P2RPP1P/1R4K1 w - - 0 26"; // Kasparov v Ribli 1989
+  // [2023-08-31 20-ply in 1:54:41 @1.366 MHz over 9.401 Gnode] eval 0.0
   // 26. Rxb5 Bxe3 27. Rd8 Bxf2+ 28. Kxf2 Qxb5 29. Qd6 Qb5f5+ 30. Kg1 Qb1+
-  // 31. Kg2 Qe4+ 32. Kh3 {with 14-ply needed to avoid checks} 32... Qf5+ 33. g4 Qf1+ 34. Kg3 Qe1+ (34... Qg1+ 35. Kf3 Qf1+ {xposing})
-  // 35. Kf3 Qf1+ 36. Ke3 Qh3+ 37. Kd4 Qxg4+ 38. Kc3 Qh3+ 39. Kb2 h6 40. Qxf8+ Kh7 41. Qh8+ Kg6 42. Rg8
+  // 31. Kg2 Qe4+ 32. Kh3! {with 14-ply needed to avoid checks} 32... Qf5+
+  //"3R1rk1/5ppp/p2Qp3/8/8/6P1/P3P2P/1q4K1 w - - 0 31"; // Kasparov v Ribli 1989 Ending
+  // [2023-08-31 21-ply in 24:41 @1.141 MHz over 1.691 Gnode] eval 4.08 after:
+  // 33. g4 Qf1+ 34. Kg3 Qe1+ (34... Qg1+ 35. Kf3 Qf1+ {xposing}) 35. Kf3 Qf1+ 36. Ke3 Qh3+ 37. Kd4 Qxg4+ 38. Kc3 h5
+  // (38... Qh3+ 39. Kb2 h6 40. Qxf8+ Kh7 41. Qh8+ Kg6 42. Rg8)
+  // 39. Rxf8+ Kh7 40. Rxf7 Qxe2 41. Qg3 Qg4 42. Qxg4 hxg4 43. Re7 g3 44. hxg3
   //"r1b1r1k1/ppq2p1p/3b2pQ/3pn3/8/2P4P/PPBN1PP1/R1B1R1K1 b - -"; // Hit Rate 1 15.8% hits [plus 1.1% Qxnt] at 12-ply, expecting ~25%
   //"6k1/5p1p/P1pb1nq1/6p1/3P4/1BP2PP1/1P1Nb2P/R1B3K1 b - -"; // Hit Rate 2 26.6% at 12-ply [plus 4% Qxnt], exceeding ~30% Hits
   //"8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - -"; //[Fine 70]87% Hits at 37-ply, should solve by 26-ply
