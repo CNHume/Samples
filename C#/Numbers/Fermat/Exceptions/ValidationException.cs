d40c6452418509bfb578c062de431026ff305d7a
Christopher Hume <cnhume-software@yahoo.com>
2025-10-26 01:22:04 -0700

    Corrected copyright notices.  Updated package references.

--- C#/Numbers/Fermat/Exceptions/ValidationException.cs
+++ C#/Numbers/Fermat/Exceptions/ValidationException.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (C) 2020, Christopher N. Hume.  All rights reserved.
+// Copyright (C) 2019-2025, Christopher N. Hume.  All rights reserved.
 //
 namespace Fermat.Exceptions;
 
--- C#/Numbers/Fermat/Program.cs
+++ C#/Numbers/Fermat/Program.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (C) 2020, Christopher N. Hume.  All rights reserved.
+// Copyright (C) 2019-2025, Christopher N. Hume.  All rights reserved.
 //
 // You should have received a copy of the MIT License along with this program.
 // If not, see https://opensource.org/licenses/MIT.
